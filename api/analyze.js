7b8ab5e5e3099cc7cfc7a03eef70c9de6dc83af8
aileadx10-boop <ai.leadx10@gmail.com>
2026-03-03 20:36:10 +0200

    Create analyze.js

---
+++ api/analyze.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send("Method not allowed");
+  }
+
+  res.status(200).json({ message: "API working" });
+}
